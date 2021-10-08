28e7bcb5a8f509f00d4a029dba8468927652cf54
Evgenii_Efimchik <evgenii_efimchik@epam.com>
2021-10-08 17:38:54 +0300

    it 2 update

--- src/main/java/com/efimchick/springtutorial/core/it2/inputargs/Main.java
+++ src/main/java/com/efimchick/springtutorial/core/it2/inputargs/Main.java
@@ -1,9 +1,7 @@
 package com.efimchick.springtutorial.core.it2.inputargs;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.deepToString(args));
+        System.out.println("Hello, " + String.join(" ", args));
     }
 }
